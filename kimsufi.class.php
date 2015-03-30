30dd47e977da9fb7845f8154abe597125f0e16ba
Maxime Daniel <root@maxux.net>
2015-03-30 19:23:20 +0200

    new version with right api request

--- kimsufi.class.php
+++ kimsufi.class.php
@@ -1,20 +1,16 @@
 <?php
 class Kimsufi {
 	private $models = array(
-		'KS-1'     => '%7B%22gamme%22%3A%22150sk10%22%7D',
-		'KS-2'     => '%7B%22gamme%22%3A%22150sk20%22%7D',
-		'KS-2 SSD' => '%7B%22gamme%22%3A%22150sk22%22%7D',
-		'KS-3'     => '%7B%22gamme%22%3A%22150sk30%22%7D',
-		'KS-4'     => '%7B%22gamme%22%3A%22150sk40%22%7D',
-		'KS-5'     => '%7B%22gamme%22%3A%22150sk50%22%7D',
-		'KS-6'     => '%7B%22gamme%22%3A%22150sk60%22%7D',
+		'KS-1'     => '150sk10',
+		'KS-2'     => '150sk20',
+		'KS-2 SSD' => '150sk22',
+		'KS-3'     => '150sk30',
+		'KS-4'     => '150sk40',
+		'KS-5'     => '150sk50',
+		'KS-6'     => '150sk60',
 	);
 
-	private $base = 'https://ws.ovh.com/dedicated/r2/ws.dispatcher/'.
-	                'getElapsedTimeSinceLastDelivery?'.
-	                'callback=Request.JSONP.request_map.request_1&params=';
-
-	private $url    = null;
+	private $url    = 'https://ws.ovh.com/dedicated/r2/ws.dispatcher/getAvailability2';
 	private $model  = null;
 	private $apikey = null;
 	
@@ -24,11 +20,14 @@ class Kimsufi {
 		if(!isset($this->models[$model]))
 			die("[-] model not found\n");
 			
-		$this->url    = $this->base.$this->models[$model];
 		$this->apikey = $key;
 		$this->model  = $model;
 		
-		$this->check();
+		if($this->check()) {
+			echo "[+] kimsufi available !\n";
+			$this->notify();
+			
+		} else echo "[-] kimsufi unavailable\n";
 	}
 	
 	function check() {
@@ -37,36 +36,38 @@ class Kimsufi {
 		if(!($data = file_get_contents($this->url)))
 			die("[-] download failed\n");
 
-		$data = substr($data, 36);
-		$data = substr($data, 0, -2);
 		$data = json_decode($data);
-
-		if($this->debug)
-			print_r($data);
-
-		$last = time() - $data->answer;
-
-		echo "[+] last date: ";
-		echo date("d/m/Y H:i:s\n", $last);
 		
-		if(!is_numeric($data->answer) || $data->answer < 1000) {
-			echo "[+] kimsufi available !\n";
-			$this->notify();
+		foreach($data->answer->availability as $item)
+			if($item->reference == $this->models[$this->model])
+				return $this->parse($item);
 		
-		} else echo "[-] kimsufi unavailable\n";
+		die("[-] kimsufi not found\n");
+	}
+	
+	function parse($item) {
+		foreach($item->zones as $zone)
+			if($zone->availability != 'unknown')
+				return true;
+
+		return false;
 	}
 	
 	function notify() {
 		echo "[+] sending notification\n";
 		
-		$post = '{"type": "note", "title": "Kimsufi", "body": "'.$this->model.'"}';
+		$data = array(
+			'type'  => 'note',
+			'title' => $this->model,
+			'body'  => $this->model.' available'
+		);
 		
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, 'https://api.pushbullet.com/v2/pushes'); 
 		curl_setopt($ch, CURLOPT_HEADER, false); 
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 		
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 			'Content-Type: application/json',
